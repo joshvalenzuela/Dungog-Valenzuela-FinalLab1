c9f1ab2703f6c4882190c3250f083b32b84ba7ef
Kirk Dungog <kirkdungog23@gmail.com>
2026-04-24 21:39:49 +0800

    Added comments for explanations

--- logout.php
+++ logout.php
@@ -1,11 +1,12 @@
 <?php
+// Start the session
 session_start();
 
-// Unset all session variables
+// Destroy all session data
 session_unset();
-// Destroy the session
 session_destroy();
 
 // Redirect to login page
 header("Location: index.php");
 exit();
+?>
